02ab0246c1caf3c60e7b6af76cc47c38130d0dcf
emirgaraev <emir.garaev@inbox.ru>
2023-11-26 21:18:11 +0600

    db

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -61,10 +61,15 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Uchiha89',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
