0ef9d625551b158438fc12e37dd416160ed71e67
emirgaraev <emir.garaev@inbox.ru>
2023-12-14 18:35:25 +0600

    permissions

--- hospital/patient/models.py
+++ hospital/patient/models.py
@@ -1,19 +1,18 @@
 from django.db import models
+import datetime
 
 class Patient(models.Model):
-
-    GENDERS_CHOISE = (
+    GENDER_CHOICES = (
         ('MALE', 'Male'),
         ('FEMALE', 'Female'),
-        ('OTHER', 'Other')
+        ('OTHER', 'Other'),
     )
 
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     contact_info = models.CharField(max_length=255)
     date_of_birth = models.DateField()
-    gender = models.CharField(choices=GENDERS_CHOISE, max_length=20),
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=20)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
-    
